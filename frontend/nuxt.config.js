d60e3a3a9db6c274fa13b98bd16bdd51b5830ee5
Hizukuri <heartlessheartfez@gmail.com>
2021-04-05 11:53:06 +0900

    タイトルを修正

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -3,8 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - frontend',
-    title: 'frontend',
+    title: '練習用Webアプリケーション',
     htmlAttrs: {
       lang: 'ja',
     },
