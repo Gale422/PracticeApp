cc02e65e9d51f8d1de0a3ea6ea8b585e55caa678
Hizukuri <heartlessheartfez@gmail.com>
2021-04-05 10:59:25 +0900

    lintで自動修正できるように修正

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,14 +2,34 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
   ],
   plugins: [
+    // *.vueファイルをlintにかけるために必要?
+    'vue',
+    'prettier',
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    // タグが1つで完結してもOK
+    'vue/html-self-closing': 'off',
+    // タグの最後で開業しない
+    // 'vue/html-closing-bracket-newline': [2, { multiline: 'never' }],
+    // 不要なカッコは消す
+    'no-extra-parens': 1,
+    // 無駄なスペースは削除
+    'no-multi-spaces': 2,
+    'prettier/prettier': [
+      'error',
+      {
+        // aタグの羅列などで変な風にならないようにする
+        htmlWhitespaceSensitivity: 'ignore',
+      },
+    ],
+  },
 }
--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -6,25 +6,21 @@ export default {
     titleTemplate: '%s - frontend',
     title: 'frontend',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -34,13 +30,13 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -59,13 +55,12 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
