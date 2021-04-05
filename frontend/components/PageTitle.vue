f47ad5dd7ec3e6e9c171bd202ca92b48e82103db
Hizukuri <heartlessheartfez@gmail.com>
2021-04-05 14:30:50 +0900

    ページのタイトルを別コンポーネントに切り出し

---
+++ frontend/components/PageTitle.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row justify="start" align="start">
+      <v-col>
+        <h1 class="pageTitle">{{ value }}</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    value: {
+      type: String,
+      default: null,
+      required: true,
+    },
+  },
+})
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,11 +1,26 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12">
-      <h1>Home</h1>
-    </v-col>
-  </v-row>
+  <v-container>
+    <PageTitle value="Home" />
+    <v-row>
+      <v-col>
+        {{ test() }}
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+import Vue from 'vue'
+import PageTitle from '../components/PageTitle.vue'
+
+export default Vue.extend({
+  components: {
+    PageTitle,
+  },
+  methods: {
+    test(): string {
+      return 'Hello World'
+    },
+  },
+})
 </script>
