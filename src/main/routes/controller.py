233e1732f132bb2e31f83e756be161bac2ab3886
Hizukuri <heartlessheartfez@gmail.com>
2021-04-06 18:21:22 +0900

    フロントエンドにテスト用のjsonデータを渡せるようにする

--- src/main/routes/controller.py
+++ src/main/routes/controller.py
@@ -1,16 +1,26 @@
+import json
+from pathlib import Path
+
 import tornado.web
+
 
 class BaseHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "http://localhost:3000")
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
 
+
 class MainHandler(BaseHandler):
     def prepare(self):
         self.render("index.html")
+
 
 class DataHandler(BaseHandler):
     def get(self):
         search = self.get_argument('search', None)
         print("search={}".format(search))
-        self.write('{検索結果: [{test1}, {test2}]}')
+        file_path = Path(__file__).parents[3] / 'contents' / 'testData.json'
+        file_data = open(file_path, 'r')
+        data_list = json.load(file_data)
+        print('data_list: {}'.format(json.dumps(data_list,indent=2)))
+        self.write(json.dumps(data_list))
