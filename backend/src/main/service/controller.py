170e10b9277e1cfe9645fb6b45fb779c855160ba
Hizukuri <heartlessheartfez@gmail.com>
2021-04-21 18:20:49 +0900

    DBからデータを取得するようになったため、不要な実装を削除

--- backend/src/main/service/controller.py
+++ backend/src/main/service/controller.py
@@ -13,10 +13,5 @@ class DataHandler(BaseHandler):
     def get(self):
         # search = self.get_argument('search', None)
         # print("search={}".format(search))
-        file_path = Path(__file__).parents[3] / 'contents' / 'testData.json'
-        file_data = open(file_path, 'r')
-        data_list = json.load(file_data)
         result = DbConnector().getAll()
-        # print('data_list: {}'.format(json.dumps(data_list,indent=2)))
-        # self.write(json.dumps(data_list))
         self.write(json.dumps(result))
