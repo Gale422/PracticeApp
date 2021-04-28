936c7224bae51f5419fc75585f8a075fbc098f67
Hizukuri <heartlessheartfez@gmail.com>
2021-04-28 13:35:41 +0900

    タイトルの受け取り方を修正

--- frontend/components/PageTitle.vue
+++ frontend/components/PageTitle.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-row justify="start" align="start">
       <v-col>
-        <h1 class="pageTitle">{{ value }}</h1>
+        <h1 class="pageTitle"><slot /></h1>
       </v-col>
     </v-row>
   </v-container>
@@ -11,13 +11,5 @@
 <script lang="ts">
 import Vue from 'vue';
 
-export default Vue.extend({
-  props: {
-    value: {
-      type: String,
-      default: null,
-      required: true,
-    },
-  },
-});
+export default Vue.extend({});
 </script>
